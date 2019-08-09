5db0c0f33ee2a90621d623cfbc26141597bb1c01
somberSomni <kartunehustla@gmail.com>
2019-08-09 16:25:24 -0500

    working on animation for rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
+import anime from 'animejs';
+import getArc from '../helpers/angles';
 
 const Rate = styled.h4`
     border-radius: 50%;
@@ -20,14 +22,50 @@ export default function Rating({ rating }) {
 }
 
 export function VisualRating({ rating }) {
-    const canvasRef = useCallback(() => { });
+    const [color, setColor] = useState('');
+    const arcRef = useCallback(node => { 
+        if(node) {
+            anime({
+                targets: node,
+                strokeDashoffset: [anime.setDashoffset, 0],
+                easing: 'easeInOutSine',
+                duration: 1500,
+                delay: 10,
+                direction: 'alternate',
+                loop: 1
+              });
+        }
+        console.log(node);
+    }, []);
+    const width = 100;
+    const x = 50;
+    const y = 50;
+    const angle = 80;
+    const radius = (width - 10) / 2;
     return (
         <div>
             <RatingContainer>{rating}</RatingContainer>
-            <canvas
-                width={100}
-                height={100}
-                ref={canvasRef}></canvas>
+            <svg
+                width={width}
+                height={width}>
+                xmlns="http://www.w3.org/2000/svg">
+            { angle === 360 ?
+                <circle 
+                    ref={arcRef}
+                    cx={x} 
+                    cy={y} 
+                    r={radius} 
+                    stroke="black" 
+                    fill="rgba(0,0,0,0)"
+                    strokeWidth="4"/> :
+                 <path
+                ref={arcRef}
+                d={getArc(x,y,radius, 0, angle)}
+                stroke="black"
+                fill="rgba(0,0,0,0)"
+                strokeWidth="4"></path>
+            }
+            </svg>
         </div>
     )
 }
